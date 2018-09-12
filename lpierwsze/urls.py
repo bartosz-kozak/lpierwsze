51472ac7f1c0b15185982fa53a64235bae8febe6
bartosz-kozak <konas13@gmail.com>
2018-09-12 19:02:48 +0000

    Poprawiłem url dla strony startowej

--- lpierwsze/urls.py
+++ lpierwsze/urls.py
@@ -18,8 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path(' ',views.home,name='home'),
+    path('',views.home,name='home'),
     path('lpierwsze/',views.lpierwsze,name='lpierwsze'),
     path('liczby/', views.liczby,name='liczby'),
 ]
