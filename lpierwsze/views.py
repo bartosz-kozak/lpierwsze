fde715a7665781900a6aff79893f4dc2bc59dd5d
Bartosz_Kozak <bartosz.l.kozak@gmail.com>
2018-09-15 13:47:44 +0200

    Poprawiłem wygląd i funkcjonowanie strony dodałem navbar i zastosowałem templateinheritatnt.

--- lpierwsze/views.py
+++ lpierwsze/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
+
+rok = str(datetime.datetime.now().year)
 
 def home(request):
-    return render(request, 'home.html',{'tytul':'Znajdź liczby pierwsze'})
+    return render(request, 'home.html',{'tytul':'Znajdź liczby pierwsze','rok':rok})
 
 def lpierwsze(request):
     n = 0 # zmienna do obliczenia liczby liczb pierwszej
@@ -10,7 +13,7 @@ def lpierwsze(request):
     p1 = request.GET['p1']
     p2 = request.GET['p2']
     if len(p1) == 0 or len(p2) == 0:
-        return render(request, 'error.html',{'tytul':'Błąd'})
+        return render(request, 'error.html',{'tytul':'Błąd','rok':rok})
     if 'e' in p1 or 'e' in p2:
         p1 = float(p1)
         p2 = float(p2)
@@ -18,9 +21,9 @@ def lpierwsze(request):
     p1 = int(p1)
     p2 = int(p2)
     if p1 <= 1 or p2 <= 1:
-        return render(request, 'error.html',{'tytul':'Błąd'})
+        return render(request, 'error.html',{'tytul':'Błąd','rok':rok})
     if p1 > 10000 or p2 > 10000:
-        return render(request, 'error.html',{'tytul':'Błąd'})
+        return render(request, 'error.html',{'tytul':'Błąd','rok':rok})
     else:
         if p1 > p2:
             p3 = p1
@@ -42,9 +45,9 @@ def lpierwsze(request):
                         d = 1
                         l.append(a)
 
-        return render(request, 'lpierwsze.html',{'p1':p1,'p2':p2,'n':n,'l':l,'tytul':'Liczby pierwsze w przedziale'})
+        return render(request, 'lpierwsze.html',{'p1':p1,'p2':p2,'n':n,'l':l,'tytul':'Liczby pierwsze w przedziale','rok':rok})
 
 def liczby(request):
-    return render(request, 'liczby.html',{'tytul':'O liczbach pierwszych'})
+    return render(request, 'liczby.html',{'tytul':'O liczbach pierwszych','rok':rok})
 #def ulam(request):
     
